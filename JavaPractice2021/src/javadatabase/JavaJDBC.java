a3aeada69120085400f42ab0a4d5bac3b2a21459
unknown <gaikwad.anirudha@gmail.com>
2021-09-06 09:20:25 +0530

    updateJDBC

---
+++ JavaPractice2021/src/javadatabase/JavaJDBC.java
@@ -0,0 +1,95 @@
+/**
+ * 
+ */
+package javadatabase;
+
+import java.sql.*;
+
+/**
+ * @author Vaishnavi
+ *
+ */
+
+
+
+
+
+
+
+
+public class JavaJDBC {
+
+	/**
+	 * @param args
+	 */
+	
+	
+	
+	void fetchData()
+	{
+		try {
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection con =DriverManager.getConnection("jdbc:mysql:/localhost:3306/test","root","password");
+		Statement s =con.createStatement();
+		
+		ResultSet rs=s.executeQuery("select * from student");
+		//PreparedStatement pst=con.prepareStatement("select * from student");
+		//pst.executeUpdate();
+		while(rs.next())
+		{
+			System.out.println(rs.getInt(1)+" "+rs.getString(2));
+						
+		}
+		
+		con.close();
+		
+		
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		
+	}
+	
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+		try {
+			
+			Class.forName("com.mysql.jdbc.Driver");
+			
+			Connection con =DriverManager.getConnection("jdbc:mysql:/localhost:3306/test","root","password");
+			
+			Statement s =con.createStatement();
+			PreparedStatement pst=con.prepareStatement("inset intp student values(?,?)");
+			
+			pst.setInt(1, 104);
+			pst.setString(2, "Anirudha");
+			pst.executeUpdate();
+				
+			con.close();
+			
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+			
+		}
+		
+		
+		
+		
+
+	} //end main
+
+}// end class
+
+
+
+
